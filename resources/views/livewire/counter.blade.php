5904edc43b561b404ee021bf323630b5bfa61f6b
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-16 14:48:47 +0800

    Use layouts.app & jetstream components

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,18 @@
 <div>
-    <h1>{{ $count }}</h1>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Counter') }}
+        </h2>
+    </x-slot>
 
-    <button wire:click="increment">+</button>
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 flex justify-center items-center gap-2">
+            <x-button wire:click="increment">+</x-button>
 
-    <button wire:click="decrement">-</button>
+            <x-button wire:click="decrement">-</x-button>
+
+            <span class="text-2xl">{{ $count }}</span>
+        </div>
+    </div>
+
 </div>
