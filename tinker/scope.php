c354eaf5988357ca05181e2d0a2ba9c256dfdb9b
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-16 09:44:40 +0800

    Update scope

--- tinker/scope.php
+++ tinker/scope.php
@@ -2,28 +2,32 @@
 
 use App\Models\User;
 
-$active_users = User::where('is_active', true)->count();
+// $active_users = User::where('is_active', true)->count();
 
-echo "total active users without scope: $active_users" . PHP_EOL;
+// echo "total active users without scope: $active_users" . PHP_EOL;
 
-$inactive_users = User::where('is_active', false)->count();
+// $inactive_users = User::where('is_active', false)->count();
 
-echo "total inactive users without scope: $inactive_users" . PHP_EOL;
+// echo "total inactive users without scope: $inactive_users" . PHP_EOL;
 
-echo PHP_EOL;
+// echo PHP_EOL;
 
-$active_users = User::active()->count();
+// $active_users = User::active()->count();
 
-echo "total active users with local scope: $active_users" . PHP_EOL;
+// echo "total active users with local scope: $active_users" . PHP_EOL;
 
-$inactive_users = User::inActive()->count();
+// $inactive_users = User::inActive()->count();
 
-echo "total inactive users without local scope: $inactive_users" . PHP_EOL;
+// echo "total inactive users without local scope: $inactive_users" . PHP_EOL;
 
 
-$search_results = User::search('Dr.')->count();
+// $search_results = User::search('Dr.')->count();
 
-echo "total search result : $search_results" . PHP_EOL;
-// $users = User::query()->get();
+// echo "total search result : $search_results" . PHP_EOL;
+
+// global scopes
+$users = User::query()->count();
+
+echo "total active users without scope: $users" . PHP_EOL;
 
 exit;
