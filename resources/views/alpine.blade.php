7ac99587e6d63422b3316b45f62aa879a4b423cf
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-18 10:04:40 +0800

    AlpineJs: Search Input

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -12,9 +12,7 @@
     <div class="flex justify-center mx-auto my-8 max-w-7xl">
         <div x-data="{ open: false }">
             <x-button @click="open = ! open">Toggle</x-button>
-
             <div x-show="open" @click.outside="open = false">
-
                 <ul class="bg-slate-50 border border-slate-700 rounded-md shadow-md my-2">
                     <li class="rounded text-center hover:bg-slate-300">
                         <a href="https://google.com">Google</a>
@@ -27,6 +25,28 @@
                     </li>
                 </ul>
             </div>
+        </div>
+    </div>
+
+    <div class="flex justify-center mx-auto my-8 max-w-7xl">
+        <div x-data="{
+            search: '',
+
+            items: ['foo', 'bar', 'baz'],
+
+            get filteredItems() {
+                return this.items.filter(
+                    i => i.startsWith(this.search)
+                )
+            }
+        }">
+            <input x-model="search" placeholder="Search..." class="rounded-md shadow-md">
+
+            <ul class="bg-slate-50 border border-slate-700 rounded-md shadow-md my-2">
+                <template x-for="item in filteredItems" :key="item">
+                    <li class="rounded text-center hover:bg-slate-300" x-text="item"></li>
+                </template>
+            </ul>
         </div>
     </div>
 </x-guest-layout>
