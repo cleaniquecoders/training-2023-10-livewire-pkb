1dc93c1c4790e5a4106f29b564c16fce9d370229
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-18 09:50:28 +0800

    AlpineJs: Toggle

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -1,10 +1,31 @@
 <x-guest-layout>
     <div class="flex justify-center mx-auto my-8 max-w-7xl">
         <div x-data="{ count: 0 }">
-            <p class="text-center flex" x-text="count"></p>
-            <div>
+            <p class="text-center w-full" x-text="count"></p>
+            <div class="space-4">
                 <x-button x-on:click="count++">Increment</x-button>
                 <x-button x-on:click="count--">Decrement</x-button>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex justify-center mx-auto my-8 max-w-7xl">
+        <div x-data="{ open: false }">
+            <x-button @click="open = ! open">Toggle</x-button>
+
+            <div x-show="open" @click.outside="open = false">
+
+                <ul class="bg-slate-50 border border-slate-700 rounded-md shadow-md my-2">
+                    <li class="rounded text-center hover:bg-slate-300">
+                        <a href="https://google.com">Google</a>
+                    </li>
+                    <li class="rounded text-center hover:bg-slate-300">
+                        <a href="https://google.com">Google</a>
+                    </li>
+                    <li class="rounded text-center hover:bg-slate-300">
+                        <a href="https://google.com">Google</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
