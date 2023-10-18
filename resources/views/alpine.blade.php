e29588c7aa12a80d02aef7b5f214efc9be6ada16
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-18 10:44:43 +0800

    AlpineJs: Accordion

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -51,7 +51,7 @@
     </div>
 
 
-    <div class="flex justify-center">
+    <div class="flex justify-center mx-auto my-8 max-w-7xl">
         <div x-data="{
             open: false,
             toggle() {
@@ -112,6 +112,46 @@
                 <x-alpine.dropdown-link>Edit Task</x-alpine.dropdown-link>
                 <x-alpine.dropdown-link url="https://google.com">Google</x-alpine.dropdown-link>
             </x-alpine.dropdown>
+        </div>
+
+
+    </div>
+
+    <div class="flex justify-center mx-auto my-8 max-w-7xl">
+        <div x-data="{ open: false }" class="bg-white mx-4 my-2 w-1/2 p-4 shadow-md space-y-4 max-w-7xl">
+
+            <div class="flex justify-between space-x-2 ">
+                <span class="text-3xl">Question 1</span>
+                {{-- <x-button @click="open = ! open">Toggle</x-button> --}}
+                <span x-show="open" aria-hidden="true" @click="open = false"
+                    class="ml-4 text-2xl cursor-pointer hover:bg-slate-300 hover:text-slate-700 hover:rounded hover:shadow-sm px-4 hover:text-white">&minus;</span>
+                <span x-show="!open" aria-hidden="true" @click="open = true"
+                    class="ml-4 text-2xl cursor-pointer hover:bg-slate-300 hover:text-slate-700 hover:rounded hover:shadow-sm px-4 hover:text-white">&plus;</span>
+            </div>
+
+            <div x-show="open">
+                <p class="my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum tincidunt
+                    gravida. Etiam vel consectetur purus. Donec tincidunt id ligula ut accumsan. Sed semper hendrerit
+                    justo, at dapibus velit interdum sed. Sed in eros sit amet elit aliquet varius non placerat nisl.
+                    Integer congue mi vitae lectus malesuada, eget tempor nisi fringilla. Vestibulum laoreet nisi ut
+                    sapien sollicitudin vehicula. Integer eget justo venenatis, ullamcorper nulla in, rutrum leo. Donec
+                    id convallis lacus, vitae pharetra elit.</p>
+
+                <p class="my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum tincidunt
+                    gravida. Etiam vel consectetur purus. Donec tincidunt id ligula ut accumsan. Sed semper hendrerit
+                    justo, at dapibus velit interdum sed. Sed in eros sit amet elit aliquet varius non placerat nisl.
+                    Integer congue mi vitae lectus malesuada, eget tempor nisi fringilla. Vestibulum laoreet nisi ut
+                    sapien sollicitudin vehicula. Integer eget justo venenatis, ullamcorper nulla in, rutrum leo. Donec
+                    id convallis lacus, vitae pharetra elit.</p>
+
+                <p class="my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum tincidunt
+                    gravida. Etiam vel consectetur purus. Donec tincidunt id ligula ut accumsan. Sed semper hendrerit
+                    justo, at dapibus velit interdum sed. Sed in eros sit amet elit aliquet varius non placerat nisl.
+                    Integer congue mi vitae lectus malesuada, eget tempor nisi fringilla. Vestibulum laoreet nisi ut
+                    sapien sollicitudin vehicula. Integer eget justo venenatis, ullamcorper nulla in, rutrum leo. Donec
+                    id convallis lacus, vitae pharetra elit.</p>
+            </div>
+
         </div>
     </div>
 </x-guest-layout>
